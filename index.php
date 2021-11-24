26fd7e7f14865e2c7fe1ff80c7cc4526baa2f0d1
Julius Hernandez <julius_hernandez@apple.com>
2021-11-23 19:11:49 -0800

    html5 template code
    this is the initial template user interface code I bought, i'll do the backend web development around this frontend

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * From this point on (8-28-2020@8:28pm) This project will be used primarily to
+ * prepare for the ZCE exam I'm going to take on 9-8-2020@5pm
+ *
+ * This front controller is called by the App Engine web server to handle all
+ * incoming requests. To change this, you will need to modify the "entrypoint"
+ * directive in `app.yaml`.
+ * @see https://cloud.google.com/appengine/docs/standard/php/config/appref
+ */
+
+echo 'hi <br>';
+
+// 'Unify/html/index.html'
+
+
+// /Users/juliushernandez/Documents/GitHub/template-designs/cloud/Unify/html/index.html
+ ?>
+
+<iframe src='cloud/Unify/html/index.html'/ width='100%' height='100%'>
